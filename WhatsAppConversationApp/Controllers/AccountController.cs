dcd25bf53cafe1ba9d02749022f9e47c01c5f44c
AM <engrabdulmateen@outlook.com>
2020-10-15 16:52:17 +0500

    account creted

---
+++ WhatsAppConversationApp/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WhatsAppConversationApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult create(string name, string username, string password, string )
+        {
+
+        }
+    }
+}
