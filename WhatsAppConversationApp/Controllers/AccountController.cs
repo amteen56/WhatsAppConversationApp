4ede45623fd73491156d60870b888e95b062c4e9
AM <engrabdulmateen@outlook.com>
2020-10-15 17:36:11 +0500

    user data model

--- WhatsAppConversationApp/Controllers/AccountController.cs
+++ WhatsAppConversationApp/Controllers/AccountController.cs
@@ -1,22 +1,24 @@
-﻿using System;
+﻿using CRM.Database;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WhatsAppConversationApp.Model;
 
 namespace WhatsAppConversationApp.Controllers
 {
     public class AccountController : Controller
     {
+        string error = "";
         // GET: Account
         public ActionResult Index()
         {
             return View();
         }
 
-        public JsonResult create(string name, string username, string password, string )
-        {
 
-        }
     }
 }
---
+++ WhatsAppConversationApp/Model/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WhatsAppConversationApp.Model
+{
+    public class User
+    {
+        public string name;
+        public string username;
+        public string password;
+        public string lsat_login;
+        public int user_id;
+    }
+}
